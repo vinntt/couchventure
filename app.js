4d149ebb88ed6c0dfdbb120f78aeb8f3267eb097
vinntt <vinh.ngtt@gmail.com>
2022-03-11 00:17:36 +0100

    Serve static files in express

--- app.js
+++ app.js
@@ -40,6 +40,13 @@ const searchRouter = require("./routes/search");
 app.use("/search", isAuthenticated, searchRouter);
 
 // const userRouter = require("./routes/")
+const path = require('path');
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.use((req, res) => {
+    // If no routes match, send them the React HTML.
+    res.sendFile(__dirname + "/client/build/index.html");
+});
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
